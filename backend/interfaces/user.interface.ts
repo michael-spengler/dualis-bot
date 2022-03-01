1b3cfc7d663de53cfaafaa43c02ef0bbf83e341a
lumaghg <rotkehlchen567@gmail.com>
2022-03-01 18:16:49 +0100

    completed user Schema

--- backend/interfaces/user.interface.ts
+++ backend/interfaces/user.interface.ts
@@ -1,15 +1,32 @@
 import {
-    Bson,
-  } from "https://deno.land/x/mongo@v0.29.2/mod.ts";
-  import Utils from "../utils/utils.ts"
+  Bson,
+} from "https://deno.land/x/mongo@v0.29.2/mod.ts";
+import Utils from "../utils/utils.ts"
 
 export interface IUser {
-    _id: Bson.ObjectId;
-    username: string;
-    password: string;
-    dualis_username: string;
-    dualis_password: string;
-    active: boolean;
+  _id: Bson.ObjectId;
+  username: string;
+  password: string;
+  dualis_username: string;
+  dualis_password: string;
+  active: boolean,
+  notifications: {
+    email: {
+      notificationEmail: string,
+      withGrades: boolean,
+      active: boolean
+    },
+    discord: {
+      notificationEmail: string,
+      withGrades: boolean,
+      active: boolean
+    },
+    telegram: {
+      notificationEmail: string,
+      withGrades: boolean,
+      active: boolean
+    }
   }
-  export default (await Utils.getDatabaseClient()).database("myFirstDatabase").collection<IUser>("users");
-  
+}
+export default (await Utils.getDatabaseClient()).database("myFirstDatabase").collection<IUser>("users");
+
--- backend/schemas/user.schema.ts
+++ backend/schemas/user.schema.ts
@@ -1,10 +1,28 @@
-import {Schema} from "https://deno.land/x/valivar/mod.ts"
+import {Schema} from "https://deno.land/x/valivar@v6.2.11/mod.ts"
 
 
-let userSchema = new Schema({
+const userSchema = new Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
     dualis_username: { type: String, required: true },
-    dualis_password: { type: String, required: true }
+    dualis_password: { type: String, required: true },
+    active: {type: Boolean, required: true, default: true},
+    notifications: {
+        email: {
+            notificationEmail: {type:String},
+            withGrades: {type: Boolean},
+            active: {type: Boolean}
+        },
+        discord: {
+            notificationUsername: {type:String},
+            withGrades: {type: Boolean},
+            active: {type: Boolean}
+        },
+        telegram: {
+            notificationUsername: {type:String},
+            withGrades: {type: Boolean},
+            active: {type: Boolean}
+        }
+    }
 })
 export default userSchema
