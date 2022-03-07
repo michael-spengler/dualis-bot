b9d3a8d0e91892100e92439a5bd9b3fbfb8e9c55
Steffen Huels <steffen.huels@sap.com>
2022-03-07 16:22:47 +0100

    added bot token to .env, improved message layout and small code fixes

--- backend/notifications/telegram.ts
+++ backend/notifications/telegram.ts
@@ -3,37 +3,37 @@ import {IDualisCourse} from "../interfaces/dualis.interface.ts"
 import {Request} from 'https://deno.land/x/request@1.3.0/request.ts'
 
 export async function sendMessage(targetID: string, message: string) {
-    const telegramBotToken = "5154060120:AAG1vr7GrGPgImJ_j8gVRTVdB1btuvsfZdo"
+    const telegramBotToken = Deno.env.get("TELEGRAM_BOT")
     //sendMessage
     const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage?chat_id=${targetID}&text=${message}&disable_web_page_preview=true`
+    console.log(url)
     await Request.get(url)
 }
 
 export function getMessageFromChanges(user: IUser, dualisChanges: IDualisCourse[]){
-    let message = "Hallo, '\n'Es sind folgende neue Bewertungen in Dualis verfügbar:"
-    const amountGrades = dualisChanges.length
+    let message = "Hallo, %0AEs sind folgende neue Bewertungen in Dualis verfügbar:"
     const personalMessage = user.notifications.telegram.withGrades
     // personal Message with Grades and Submodules
     if (personalMessage) { 
         //iterate through all modules with new grades
-        for(let i = 0; i < amountGrades; i++) {
-            message += "'\n'"
+        for(let i in dualisChanges) {
+            message += "%0A"
             const totalModuleObject = dualisChanges[i].examinations;
             //iterate through submodules
-            for(let j = 0; j < totalModuleObject.length; j++){ 
+            for(let j in totalModuleObject){ 
                 const submoduleObject = totalModuleObject[j]
-                message += "'\n'" + dualisChanges[i].name + " - " + submoduleObject.exam_type + ": " + submoduleObject.grade
+                message += "%0A" + dualisChanges[i].name + " - " + submoduleObject.exam_type + ": " + submoduleObject.grade
             }
         }
     // message without grades
     } else {
         //iterate through modules with new grades
-        for(let i = 0; i < amountGrades; i++) {
-            message += "'\n'" + dualisChanges[i].name
+        for(let i in dualisChanges) {
+            message += "%0A" + dualisChanges[i].name
         }
     }
 
-    message += "\nVielen Dank für ihr Vertrauen an unseren Service. Der Dualis-Bot hält Sie immer auf dem aktuellen Stand. '/n' Besuche " + "WEBSITE" + " für die persönliche Konfiguration."
-
+    message += "%0AVielen Dank für ihr Vertrauen an unseren Service. Der Dualis-Bot hält Sie immer auf dem aktuellen Stand. %0A Besuche " + "WEBSITE" + " für die persönliche Konfiguration."
+    console.log(message)
     return message
 }
