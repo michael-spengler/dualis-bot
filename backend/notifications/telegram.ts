79d5a982899a252fbea76c6cb10f7ee24da072e8
Steffen Huels <steffen.huels@sap.com>
2022-03-09 15:31:27 +0100

    code cleanup

--- backend/notifications/telegram.ts
+++ backend/notifications/telegram.ts
@@ -1,18 +1,11 @@
-import {IUser} from "../interfaces/user.interface.ts"
-import {IDualisCourse} from "../interfaces/dualis.interface.ts"
 import {Request} from 'https://deno.land/x/request@1.3.0/request.ts'
-import "https://deno.land/x/dotenv@v3.2.0/load.ts"; //load env
 
-export async function sendMessage(targetID: string, message: string) {
-    //sendMessage
+export async function sendMessage(targetID: string, message: string, telegramBotToken: string) {
     const messageURL = `https://api.telegram.org/bot${telegramBotToken}/sendMessage?chat_id=${targetID}&text=${message}&disable_web_page_preview=true`
-    console.log(messageURL)
     await Request.get(messageURL)
 }
 
-export async function sendSticker(targetID: string, sticker: string) {
-    const telegramBotToken = Deno.env.get("TELEGRAM_BOT")
-    //sendPanicSticker
+export async function sendSticker(targetID: string, sticker: string, telegramBotToken: string) {
     const stickerURL = `https://api.telegram.org/bot${telegramBotToken}/sendSticker?chat_id=${targetID}&sticker=${sticker}`
     await Request.get(stickerURL)
 }
--- backend/utils/utils.ts
+++ backend/utils/utils.ts
@@ -72,11 +72,12 @@ export default class Utils {
         console.log("notifications not implemented complete yet", user, dualisChanges)
 
         //Telegram Notification
+        const telegramBotToken = Deno.env.get("TELEGRAM_BOT") || ""
         const targetID = user.notifications.telegram.notificationNumber //Id of user or chat https://www.alphr.com/find-chat-id-telegram/  RawDataBot
         const personalMessage = user.notifications.telegram.withGrades  //check if personal message is necessary for msg function
         //send funny sticker before serious message
-        telegram.sendSticker(targetID, "CAACAgIAAxkBAAMhYiiuBKoE0HYsdRMUzs_vWVShJH0AArkQAAIlbhhJi3IrcMj-D6YjBA")
-        telegram.sendMessage(targetID, msg.getMessageFromChanges(dualisChanges, personalMessage, "%0A")) 
+        telegram.sendSticker(targetID, "CAACAgIAAxkBAAMhYiiuBKoE0HYsdRMUzs_vWVShJH0AArkQAAIlbhhJi3IrcMj-D6YjBA", telegramBotToken)
+        telegram.sendMessage(targetID, msg.getMessageFromChanges(dualisChanges, personalMessage, "%0A"), telegramBotToken) 
         
         //Discord Notification
     }
