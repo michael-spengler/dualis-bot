23dd38c0b1354f9adb5fd386906c9a1be90bb14f
Robin Reyer <45331704+lumaghg@users.noreply.github.com>
2022-03-11 20:54:15 +0100

    trigger actions

--- backend/collections/user.collection.ts
+++ backend/collections/user.collection.ts
@@ -1,4 +1,3 @@
 import Utils from "../utils/utils.ts"
 import IUser from "../interfaces/user.interface.ts"
 export default (await Utils.getDatabaseClient()).database("myFirstDatabase").collection("users");
- 
