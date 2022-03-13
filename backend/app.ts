15c815e1198d8f4c5958b4e07a935a570e95ae96
Robin Reyer <45331704+lumaghg@users.noreply.github.com>
2022-03-13 22:40:57 +0100

    added cors (#34)
    Co-authored-by: lumaghg <rotkehlchen567@gmail.com>

--- backend/app.ts
+++ backend/app.ts
@@ -2,10 +2,11 @@ import { opine, json } from "./deps.ts";
 import "https://deno.land/x/dotenv@v3.2.0/load.ts"; //load env
 import Routes from './routes/routes.ts';
 import Utils from "./utils/utils.ts"
+import { opineCors } from "https://deno.land/x/cors@v1.2.1/mod.ts";
 const app = opine();
 
-
 app.use(json())
+app.use(opineCors())
 
 Utils.setupCronjob()
 
@@ -17,7 +18,6 @@ if (Number.isNaN(port)) {
 
 //setup mongodb
 await Utils.getDatabaseClient()
-
 app.use('/api/v1', Routes);
 
 app.listen(
