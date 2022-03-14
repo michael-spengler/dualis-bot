ab7f94a89646c58c2acc32e931cd0762cec5ba5d
stat1c <fynn.weyrich@web.de>
2022-03-14 11:29:25 +0100

    push non working unit tests

---
+++ backend/tests/mocks/djwt.mock.ts
@@ -0,0 +1,14 @@
+import {
+    spy,
+  } from "https://deno.land/x/mock@0.13.0/mod.ts";
+
+export const verify = spy(function(jwt: string, secret: string, algo: string){
+  if(jwt == "wrongJWT") {
+    throw new Error("You provided a wrong jwt");
+  } else {
+    return true;
+  }
+});
+
+export const create = function() {}
+export const decode = function() {}
