7dda9923f429760ceae4df05df3462f402839633
lumaghg <rotkehlchen567@gmail.com>
2022-04-11 12:35:35 +0200

    updated BACKEND_SERVER

--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -1,3 +1,3 @@
 import { writable } from 'svelte/store';
 export const jwt = writable('test');
-export const BACKEND_SERVER = "http://localhost:4000/api/v1"
+export const BACKEND_SERVER = "http://api.dualis-bot.robin-reyer.de/api/v1"
