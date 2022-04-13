a804ebd32223a1fa9cb851b5dd91e762e1f52d25
Steffen Huels <steffen.huels@sap.com>
2022-04-13 20:04:09 +0200

    updated testcase

--- backend/tests/specs/notifications/message.test.ts
+++ backend/tests/specs/notifications/message.test.ts
@@ -1,15 +1,29 @@
 import { assertEquals } from "../../test_deps.ts";
 import { getMessageFromChanges } from "../../../notifications/message.ts";
 
-const changeData = [{
-  "name": "Mein erster Kurs",
-  "examinations": [
-    {
-      "exam_type": "Klausur",
-      "grade": "1.2",
-    },
-  ],
-}];
+const changeData = [
+  {
+    name: "Mein erster Kurs",
+    examinations: [
+      {
+        exam_type: "Klausur",
+        grade: "4,1",
+      },
+    ],
+  },
+];
+
+const changeData2 = [
+  {
+    name: "Mein erster Kurs",
+    examinations: [
+      {
+        exam_type: "Klausur",
+        grade: "1,0",
+      },
+    ],
+  },
+];
 
 Deno.test("test message without grades", () => {
   const expected =
@@ -20,9 +34,27 @@ Deno.test("test message without grades", () => {
 });
 
 Deno.test("test message with grades", () => {
-  const expected =
-    "Hallo, \nEs sind folgende neue Bewertungen in Dualis verfügbar:\n\nMein erster Kurs - Klausur: 1.2\n\nVielen Dank für ihr Vertrauen an unseren Service. Der Dualis-Bot hält Sie immer auf dem aktuellen Stand. \nBesuche undefined für die persönliche Konfiguration.";
+  const expected = 
+   "Hallo, \nEs sind folgende neue Bewertungen in Dualis verfügbar:\n\nMein erster Kurs - Klausur: 4,1\n\nVielen Dank für ihr Vertrauen an unseren Service. Der Dualis-Bot hält Sie immer auf dem aktuellen Stand. \nBesuche undefined für die persönliche Konfiguration.",
+  
+
   const message = getMessageFromChanges(changeData, true, "\n").msg;
+
+  assertEquals(message, expected);
+});
+
+Deno.test("test message with 4,", () => {
+  const expected = true;
+
+  const message = getMessageFromChanges(changeData, true, "\n").badGrade;
+
+  assertEquals(message, expected);
+});
+
+Deno.test("test message with 1,0", () => {
+  const expected = true;
+
+  const message = getMessageFromChanges(changeData2, true, "\n").goodGrade;
 
   assertEquals(message, expected);
 });
